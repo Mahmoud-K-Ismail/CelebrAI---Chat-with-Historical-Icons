7f400feb1cf37212ac85d9ca0e5a337310d2f131
Mahmoud Kassem Ismail <117085344+Mahmoud-K-Ismail@users.noreply.github.com>
2024-11-22 19:33:35 -0500

    before fixing eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,31 +2,31 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-/** @type {import('eslint').Linter.Config} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
     },
     rules: {
-      'semi': ['error', 'always'],
-      'no-var': ['error'],
-      'prefer-const': ['error', { 'destructuring': 'any', 'ignoreReadBeforeAssign': false }],
-      'curly': ['error'],
-      'eqeqeq': ['error'],
-      'no-multi-spaces': ['error'],
-      'no-lone-blocks': ['error'],
-      'no-self-compare': ['error'],
-      'no-unused-expressions': ['error'],
-      'no-useless-call': ['error'],
-      'no-use-before-define': ['error'],
-      'camelcase': ['error', { properties: 'never' }],
-      'func-call-spacing': ['error'],
-      'no-lonely-if': ['error'],
-      'array-bracket-spacing': ['error'],
-      'no-console': ['off']
-    }
+      semi: ["error", "always"],
+      "no-var": ["error"],
+      "prefer-const": ["error", { destructuring: "any", ignoreReadBeforeAssign: false }],
+      curly: ["error"],
+      eqeqeq: ["error"],
+      "no-multi-spaces": ["error"],
+      "no-lone-blocks": ["error"],
+      "no-self-compare": ["error"],
+      "no-unused-expressions": ["error"],
+      "no-useless-call": ["error"],
+      "no-use-before-define": ["error"],
+      camelcase: ["error", { properties: "never" }],
+      "func-call-spacing": ["error"],
+      "no-lonely-if": ["error"],
+      "array-bracket-spacing": ["error"],
+      "no-console": ["off"],
+    },
   },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
